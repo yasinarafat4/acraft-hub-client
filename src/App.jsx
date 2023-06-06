df989d764f2a1f9ef4cf18ffd63f46a7f851e575
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-06-06 22:41:54 +0600

    All primary setup including router and component libraryhas done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-center">
+        Hello ACraft Lover!
+      </h1>
+      ;
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div>
+      <h2>This is Navigation Bar</h2>
+    </div>
+  );
+};
+
+export default Navbar;
