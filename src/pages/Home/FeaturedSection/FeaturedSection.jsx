ba7d794a494edfdacabce0519a690bc2fedcd77a
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-06-10 14:26:31 +0600

    Scroll top functionality added on the Get Started Button

--- src/pages/Home/FeaturedSection/FeaturedSection.jsx
+++ src/pages/Home/FeaturedSection/FeaturedSection.jsx
@@ -1,8 +1,19 @@
+import { useEffect } from "react";
 import "./FeaturedSection.css";
 
 const FeaturedSection = () => {
+  // Scroll to the top functionality
+  const handleGetStarted = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  // Scroll to the top effect
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
-    <div className="featured-item bg-fixed bg-cover text-white py-36 md:py-40 lg:py-52 xl:py-72 mt-12 ">
+    <div className="featured-item bg-fixed bg-cover text-white py-36 md:py-40 lg:py-52 xl:py-64 mt-12 ">
       <div className="text-center">
         <h2 className="text-3xl md:text-4xl lg:text-6xl xl:text-7xl">
           BECOME ONE OF US!
@@ -11,7 +22,10 @@ const FeaturedSection = () => {
           Take the first step to achieving your dreams.
         </p>
         <div className="my-6 lg:mt-6">
-          <button className="bg-[#133795] text-[#FFFFFF]  hover:bg-[#FFFFFF] hover:duration-500 hover:text-black font-semibold py-2 px-3 md:py-3 md:px-6 xl:py-4 xl:px-10 text-md lg:text-xl mt-4">
+          <button
+            onClick={handleGetStarted}
+            className="bg-[#133795] text-[#FFFFFF]  hover:bg-[#FFFFFF] hover:duration-500 hover:text-black font-semibold py-2 px-3 md:py-3 md:px-6 xl:py-4 xl:px-10 text-md lg:text-xl mt-4"
+          >
             GET STARTED
           </button>
         </div>
