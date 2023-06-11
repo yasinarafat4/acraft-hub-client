babfe3b7f0108c92e3eb70d2d1cf5c38df247a94
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-06-11 13:56:20 +0600

    Send JWT token to back end for verification done

--- src/hooks/useSelectedClasses.jsx
+++ src/hooks/useSelectedClasses.jsx
@@ -3,11 +3,17 @@ import useAuth from "./useAuth";
 
 const useSelectedClasses = () => {
   const { user } = useAuth();
+  const token = localStorage.getItem("access-token");
   const { refetch, data: selectedClass = [] } = useQuery({
     queryKey: ["selectedClasses", user?.email],
     queryFn: async () => {
       const response = await fetch(
-        `http://localhost:5000/selectedClasses?email=${user?.email}`
+        `http://localhost:5000/selectedClasses?email=${user?.email}`,
+        {
+          headers: {
+            authorization: `bearer ${token}`,
+          },
+        }
       );
       return response.json();
     },
