258f620a1b58e4220ddcff894392c7afe7fe8935
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-06-11 06:33:52 +0600

    Saving users data in the database has done

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -31,9 +31,26 @@ const SignUp = () => {
 
       updateUserProfile(data.name, data.photoURL)
         .then(() => {
-          reset();
-          Swal.fire("Good job!", "You have successfully signed up!", "success");
-          navigate("/");
+          const saveUser = { name: data.name, email: data.email };
+          fetch("http://localhost:5000/users", {
+            method: "POST",
+            headers: {
+              "content-type": "application/json",
+            },
+            body: JSON.stringify(saveUser),
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              if (data.insertedId) {
+                reset();
+                Swal.fire(
+                  "Good job!",
+                  "You have successfully signed up!",
+                  "success"
+                );
+                navigate("/");
+              }
+            });
         })
         .catch((error) => console.log(error));
     });
