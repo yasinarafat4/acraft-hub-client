a20181f21e4e6a73746fd9516a4a90cfcea0f263
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-06-14 00:26:19 +0600

    Getting Price for payment intent has done

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -2,17 +2,21 @@ import { Helmet } from "react-helmet-async";
 import CheckoutForm from "./CheckoutForm";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
+import useSelectedClasses from "../../../hooks/useSelectedClasses";
 
 const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
 
 const Payment = () => {
+  const [selectedClass] = useSelectedClasses();
+  const price = selectedClass.map((item) => item.price);
+  console.log(price);
   return (
     <div>
       <Helmet>
         <title>ACraft | Payment</title>
       </Helmet>
       <Elements stripe={stripePromise}>
-        <CheckoutForm></CheckoutForm>
+        <CheckoutForm price={price}></CheckoutForm>
       </Elements>
     </div>
   );
