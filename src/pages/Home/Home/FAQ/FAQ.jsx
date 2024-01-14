47e09b7c19d3176582fa1c0153b726e9eb47fbab
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2024-01-14 13:44:30 +0600

    FAQ section updated

--- src/pages/Home/Home/FAQ/FAQ.jsx
+++ src/pages/Home/Home/FAQ/FAQ.jsx
@@ -1,98 +1,5 @@
-// import React from "react";
-// import SectionTitle from "../../../../components/SectionTitle/SectionTitle";
-
 import { useState } from "react";
 import SectionTitle from "../../../../components/SectionTitle/SectionTitle";
-
-// const FAQ = () => {
-//   return (
-//     <div className="m-4">
-//       {/* Section Title */}
-//       <SectionTitle
-//         heading={"Frequently Asked Questions (FAQ)"}
-//         text={
-//           "Discover answers to your most common queries in our comprehensive FAQ section."
-//         }
-//       />
-//       {/* FAQ's */}
-//       <div className="collapse collapse-plus bg-base-200 mt-2">
-//         <input type="radio" name="my-accordion-3" checked="checked" />
-//         <div className="collapse-title text-xl font-medium">
-//           How can I enroll my child in an art and craft class for the summer
-//           camp?
-//         </div>
-//         <div className="collapse-content">
-//           <p>
-//             Enrolling your child is easy! Navigate to our "Classes" section,
-//             choose the desired art and craft class, and click on the "Enroll
-//             Now" button. Follow the prompts to complete the registration process
-//             securely.
-//           </p>
-//         </div>
-//       </div>
-//       <div className="collapse collapse-plus bg-base-200 mt-2">
-//         <input type="radio" name="my-accordion-3" />
-//         <div className="collapse-title text-xl font-medium">
-//           What age groups are the art and craft classes suitable for?
-//         </div>
-//         <div className="collapse-content">
-//           <p>
-//             Our art and craft classes cater to a variety of age groups. We offer
-//             classes designed for different age ranges, ensuring an engaging and
-//             age-appropriate experience for every child participating in our
-//             summer camp.
-//           </p>
-//         </div>
-//       </div>
-//       <div className="collapse collapse-plus bg-base-200 mt-2">
-//         <input type="radio" name="my-accordion-3" />
-//         <div className="collapse-title text-xl font-medium">
-//           Are there any discounts or promotions available for multiple class
-//           enrollments?
-//         </div>
-//         <div className="collapse-content">
-//           <p>
-//             Yes, we often have special promotions and discounts for families
-//             enrolling in multiple classes. Check our "Promotions" page for the
-//             latest offers or contact our customer support for personalized
-//             assistance.
-//           </p>
-//         </div>
-//       </div>
-//       <div className="collapse collapse-plus bg-base-200 mt-2">
-//         <input type="radio" name="my-accordion-3" />
-//         <div className="collapse-title text-xl font-medium">
-//           Can parents attend the art and craft classes with their children?
-//         </div>
-//         <div className="collapse-content">
-//           <p>
-//             While our classes are designed for children, we encourage parents to
-//             join specific classes or workshops designed for family
-//             participation. It's a great way to bond and create lasting memories
-//             together.
-//           </p>
-//         </div>
-//       </div>
-//       <div className="collapse collapse-plus bg-base-200 mt-2">
-//         <input type="radio" name="my-accordion-3" />
-//         <div className="collapse-title text-xl font-medium">
-//           What safety measures are in place during the summer camp sessions?
-//         </div>
-//         <div className="collapse-content">
-//           <p>
-//             The safety of our campers is our top priority. We follow strict
-//             safety protocols, including qualified instructors, first aid
-//             provisions, and a secure environment. Additionally, we adhere to any
-//             local health guidelines to ensure a safe and enjoyable experience
-//             for everyone.
-//           </p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default FAQ;
 
 const FAQ = () => {
   const [isOpen, setIsOpen] = useState(null);
@@ -102,34 +9,38 @@ const FAQ = () => {
 
   const datas = [
     {
-      title: "Connected Devices",
+      title:
+        "How can I enroll my child in an art and craft class for the summer camp?",
       color: "green",
       description:
-        'To create an account, click on the "Sign Up" button and fill out the required information. Once done, you can enjoy the benefits of being a registered member.',
+        'Enrolling your child is easy! Navigate to our "Classes" section, choose the desired art and craft class, and click on the "Enroll Now" button. Follow the prompts to complete the registration process securely.',
     },
     {
-      title: "Apps Permissions",
+      title: "What age groups are the art and craft classes suitable for?",
       color: "sky",
       description:
-        "Our return policy allows you to return items within 30 days of purchase. Please visit our returns page for detailed instructions and to initiate a return.",
+        "Our art and craft classes cater to a variety of age groups. We offer classes designed for different age ranges, ensuring an engaging and age-appropriate experience for every child participating in our summer camp.",
     },
     {
-      title: "Pending task",
+      title:
+        "Are there any discounts or promotions available for multiple class enrollments?",
       color: "purple",
       description:
-        "Yes, you can change your shipping address before your order is shipped. Go to your account settings and update the shipping information accordingly.",
+        "Yes, we often have special promotions and discounts for families enrolling in multiple classes. Stay with us for the latest offers or contact our customer support for personalized assistance.",
     },
     {
-      title: "Card expired",
+      title:
+        "Can parents attend the art and craft classes with their children?",
       color: "amber",
       description:
-        "We appreciate our loyal customers! Stay tuned for exclusive discounts, promotions, and special offers available to members of our loyalty program.",
+        " While our classes are designed for children, we encourage parents to join specific classes or workshops designed for family participation. It's a great way to bond and create lasting memories together.",
     },
     {
-      title: "Card expired",
+      title:
+        "What safety measures are in place during the summer camp sessions?",
       color: "red",
       description:
-        "We appreciate our loyal customers! Stay tuned for exclusive discounts, promotions, and special offers available to members of our loyalty program.",
+        " The safety of our campers is our top priority. We follow strict safety protocols, including qualified instructors, first aid provisions, and a secure environment. Additionally, we adhere to any local health guidelines to ensure a safe and enjoyable experience for everyone.",
     },
   ];
   return (
