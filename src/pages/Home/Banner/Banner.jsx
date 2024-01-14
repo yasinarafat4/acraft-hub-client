13dd5d4db926294eecfa04b2460990f0e1815ee4
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2024-01-14 13:32:02 +0600

    Bnner section updated

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,89 +1,77 @@
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Banner.css";
 
-import img1 from "../../../assets/images/banner/banner.jpg";
-import img2 from "../../../assets/images/banner/banner2.jpg";
-import img3 from "../../../assets/images/banner/banner4.jpg";
 
 const Banner = () => {
-  const imageStyles = {
-    maxHeight: "650px",
-  };
+  const [currentSlider, setCurrentSlider] = useState(0);
+  const sliders = [
+    {
+      img: "https://i.ibb.co/jhtBF24/banner-1.jpg",
+      title: "Art Craft Hub",
+      des: "Explore a collection of beautiful artworks and crafts with us.",
+    },
+    {
+      img: "https://i.ibb.co/3NjgFVG/banner-2.jpg",
+      title: "Unleash Your Creativity",
+      des: "Discover inspiration and ignite your imagination with our diverse range of art and craft creations.",
+    },
+    {
+      img: "https://i.ibb.co/yPg8y6L/banner-3.jpg",
+      title: "Elevate Your Space",
+      des: "Enhance your surroundings with exquisite artworks and handcrafted decor that reflect your unique style.",
+    },
+  ];
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentSlider((currentSlider) =>
+        currentSlider === sliders.length - 1 ? 0 : currentSlider + 1
+      );
+    }, 5000);
+
+    return () => clearInterval(intervalId);
+  }, [currentSlider, sliders.length]);
 
   return (
-    <div className="text-center">
-      <Carousel>
-        {/* Slider 1 */}
-        <div className="relative">
-          <img src={img1} alt="Banner 1" style={imageStyles} />
-          <div className="absolute left-0 top-0 inset-0 text-white bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] flex flex-col justify-center w-3/4 md:w-2/3 text-start ps-8">
-            <h2 className="text-xl md:text-3xl lg:text-4xl xl:text-5xl mb-1 md:mb-4 mt-16 md:mt-0">
-              Art Craft Hub
-            </h2>
-            <p className="text-sm md:text-lg mb-3 md:mb-6">
-              Explore a collection of beautiful artworks and crafts with us.
-            </p>
-            <div className="space-x-4">
-              <Link to="/classes">
-                <button className="btn-classes">Our Classes</button>
-              </Link>
-              <Link to="/Instructors">
-                {" "}
-                <button className="btn-instructors">Instructors</button>
-              </Link>
-            </div>
+    <>
+      <div
+        className="w-full h-96 md:h-[540px] lg:h-[640px] flex flex-col xl:flex-row items-center justify-center gap-5 lg:gap-10 relative bg-cover before:absolute before:bg-black/50 before:inset-0 duration-1000 transform ease-linear z-10"
+        style={{ backgroundImage: `url(${sliders[currentSlider].img})` }}
+      >
+        <div className="absolute left-0 top-0 inset-0 text-white bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] flex flex-col justify-center w-3/4 md:w-2/3 text-start ps-8">
+          <h2 className="text-xl md:text-3xl lg:text-4xl xl:text-5xl mb-1 md:mb-4 mt-16 md:mt-0">
+            {sliders[currentSlider].title}
+          </h2>
+          <p className="text-sm md:text-lg mb-3 md:mb-6">
+            {sliders[currentSlider].des}
+          </p>
+          <div className="space-x-4">
+            <Link to="/classes">
+              <button className="btn-classes">Our Classes</button>
+            </Link>
+            <Link to="/Instructors">
+              {" "}
+              <button className="btn-instructors">Instructors</button>
+            </Link>
           </div>
         </div>
+      </div>
 
-        {/* Slider 2 */}
-        <div className="relative">
-          <img src={img2} alt="Banner 1" style={imageStyles} />
-          <div className="absolute left-0 top-0 inset-0 text-white bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] flex flex-col justify-center w-3/4 md:w-2/3 text-start ps-8">
-            <h2 className="text-xl md:text-3xl lg:text-4xl xl:text-5xl mb-1 md:mb-4 mt-16 md:mt-0">
-              Unleash Your Creativity
-            </h2>
-            <p className="text-sm md:text-lg mb-3 md:mb-6">
-              Discover inspiration and ignite your imagination with our diverse
-              range of art and craft creations.
-            </p>
-            <div className="space-x-4">
-              <Link to="/classes">
-                <button className="btn-classes">Our Classes</button>
-              </Link>
-              <Link to="/Instructors">
-                {" "}
-                <button className="btn-instructors">Instructors</button>
-              </Link>
-            </div>
-          </div>
-        </div>
-
-        {/* Slider 3 */}
-        <div className="relative">
-          <img src={img3} alt="Banner 1" style={imageStyles} />
-          <div className="absolute left-0 top-0 inset-0 text-white bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] flex flex-col justify-center w-3/4 md:w-2/3 lg:w-3/4 text-start ps-8">
-            <h2 className="text-xl md:text-3xl lg:text-4xl xl:text-5xl mb-1 md:mb-4 mt-16 md:mt-0">
-              Elevate Your Space
-            </h2>
-            <p className="text-sm md:text-lg mb-3 md:mb-6">
-              Enhance your surroundings with exquisite artworks and handcrafted
-              decor that reflect your unique style.
-            </p>
-            <div className="space-x-4">
-              <Link to="/classes">
-                <button className="btn-classes">Our Classes</button>
-              </Link>
-              <Link to="/Instructors">
-                {" "}
-                <button className="btn-instructors">Instructors</button>
-              </Link>
-            </div>
-          </div>
-        </div>
-      </Carousel>
-    </div>
+      <div className="flex justify-center items-center gap-3 p-2 ease-linear duration-500">
+        {sliders.map((slide, inx) => (
+          <img
+            onClick={() => setCurrentSlider(inx)}
+            key={inx}
+            src={slide.img}
+            className={`h-[50px] min-w-[80px] ${
+              currentSlider === inx ? "border-2 border-black p-[2px]" : ""
+            } rounded-lg box-content cursor-pointer`}
+            alt={slide.title}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -62,7 +62,7 @@ const Navbar = () => {
     </>
   );
   return (
-    <div className="navbar fixed z-10 bg-opacity-30 max-w-full md:max-w-screen-xl bg-black text-white">
+    <div className="navbar fixed z-50 bg-opacity-30 max-w-full md:max-w-screen-xl bg-black text-white">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className=" lg:hidden cursor-pointer">
